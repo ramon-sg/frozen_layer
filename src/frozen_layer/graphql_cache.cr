f50a9f92b3deb48a81248c570fb35aca0cc3c93e
Ramón Soto <ddsstr@gmail.com>
2024-10-19 18:42:57 -0300

    refactor: remove unnecessary require statement

--- src/frozen_layer/graphql_cache.cr
+++ src/frozen_layer/graphql_cache.cr
@@ -1,4 +1,4 @@
-require "./graphql_cache/helpers/*"
+# require "./graphql_cache/helpers/*"
 require "./graphql_cache/controllers/*"
 require "./graphql_cache/services/*"
 require "./graphql_cache/*"
