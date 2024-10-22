3596461a91314fa57ebab9a9fc7f78f1c44fe469
Ramón Soto <ddsstr@gmail.com>
2024-10-21 22:44:41 -0300

    feat(headers): add custom application headers to responses

--- src/frozen_layer/graphql_cache/controllers/endpoint_controller.cr
+++ src/frozen_layer/graphql_cache/controllers/endpoint_controller.cr
@@ -10,6 +10,8 @@ module FrozenLayer
         ctx.exec do
           put_status(result.status)
           put_resp_header("Content-Type", response_content_type) if response_content_type
+          put_resp_header("X-Cache-App-Name:", "FrozenLayer")
+          put_resp_header("X-Cache-App-Version:", VERSION)
           send_resp(result.body)
         end
       end
