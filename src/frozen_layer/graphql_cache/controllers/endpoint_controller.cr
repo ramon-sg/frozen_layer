5d267d39464db5e99a264c7bc228fc52413a433d
Ramón Soto <ddsstr@gmail.com>
2024-11-05 17:18:07 -0300

    fix(graphql): correct response headers in GraphQL API

--- src/frozen_layer/graphql_cache/controllers/endpoint_controller.cr
+++ src/frozen_layer/graphql_cache/controllers/endpoint_controller.cr
@@ -5,11 +5,17 @@ module FrozenLayer
         request_body = ctx.request.body.try(&.gets_to_end)
 
         result = EndpointService.new.perform(request_body, ctx.request.headers)
-        response_content_type = result.headers.try(&.["Content-Type"]?)
 
         ctx.exec do
           put_status(result.status)
-          put_resp_header("Content-Type", response_content_type) if response_content_type
+
+          result.headers.try do |headers|
+            headers.each do |key, value|
+              puts "key: #{key}, value: #{value}"
+              put_resp_header(key, value)
+            end
+          end
+
           put_resp_header("x-powered-by:", "Frozen Layer #{VERSION}")
           put_resp_header("x-Cache:", result.cache? ? "HIT" : "MISS")
           send_resp(result.body)
